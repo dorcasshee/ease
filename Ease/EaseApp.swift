56a2f71c2987dc3a472fd8d9feeef30aec001e24
Dorcas Shee <dorcasshee@outlook.com>
2025-11-01 14:48:17 +0800

    Initial Commit

---
+++ Ease/EaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaseApp.swift
+//  Ease
+//
+//  Created by Dorcas Shee on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct EaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
