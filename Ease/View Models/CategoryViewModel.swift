9310394f981d66d58ad578abc7edcb4cc32810b8
Dorcas <dorcasshee@outlook.com>
2026-04-15 04:14:49 +0800

    Updated view models following changes in data models

--- Ease/View Models/CategoryViewModel.swift
+++ Ease/View Models/CategoryViewModel.swift
@@ -12,51 +12,55 @@ import SwiftData
     var name: String?
     var iconName: String?
     var isDefault: Bool = false
+    var isSystemIcon: Bool = true
+    var colorName: String?
     var transactionType: TransactionType = .expense
     var showSheet: Bool = false
     
     func createParentCategory(context: ModelContext) {
         guard let name = name, let iconName = iconName else { return }
         
-        let newCategory = Category(name: name, iconName: name, transactionType: transactionType)
+        let newCategory = ParentCategory(name: name, iconName: iconName, isSystemIcon: isSystemIcon, colorName: "eOrange", transactionType: transactionType)
         
         context.insert(newCategory)
     }
     
-    func createSubCategory(context: ModelContext, parentCategory: Category) {
+    func createSubCategory(context: ModelContext, parentCategory: ParentCategory) {
         guard let name = name, let iconName = iconName else { return }
         
-        let newCategory = Category(name: name, iconName: name, transactionType: transactionType, parentCategory: parentCategory)
+        let newCategory = SubCategory(name: name, iconName: iconName, isSystemIcon: isSystemIcon, isDefault: isDefault, colorName: parentCategory.colorName, parent: parentCategory)
         
         context.insert(newCategory)
     }
     
-    func getParentCategories(categories: [Category], transactionType: TransactionType) -> [Category] {
-        categories.filter { $0.isParent && $0.transactionType == transactionType }
+    func sortParentCategories(parents: [ParentCategory], type: TransactionType) -> [ParentCategory] {
+        parents.filter { $0.transactionType == type }
             .sorted(by: { $0.name < $1.name })
     }
     
-    func getMostFrequentCategories(categories: [Category], limit: Int = 8) -> [Category] {
-        let subCategories = categories.filter { $0.isSubCategory }
+    func getMostFrequentCategories(context: ModelContext, limit: Int = 8) throws -> [SubCategory] {
+        let descriptor = FetchDescriptor<SubCategory>(predicate: #Predicate { $0.parent?.transactionType == transactionType })
+        let subCategories = try context.fetch(descriptor)
+        
         return subCategories.sorted {
-            if $0.usageCount == $1.usageCount {
+            if $0.transactions.count == $1.transactions.count {
                 return $0.name < $1.name
             }
-            return $0.usageCount > $1.usageCount
+            return $0.transactions.count > $1.transactions.count
         }
         .prefix(limit)
         .map { $0 }
     }
     
-    func sortedSubCategories(cat: [Category]) -> [Category] {
-        cat.sorted(by: { $0.name < $1.name })
+    func sortedSubCategories(parent: ParentCategory) -> [SubCategory] {
+        parent.subCategories.sorted(by: { $0.name < $1.name })
     }
     
-    func getDefaultCategory(for transactionType: TransactionType, context: ModelContext) throws -> Category {
-        let descriptor = FetchDescriptor<Category> ( predicate: #Predicate { $0.isDefault == true })
+    func getDefaultCategory(for type: TransactionType, context: ModelContext) throws -> SubCategory {
+        let descriptor = FetchDescriptor<SubCategory> ( predicate: #Predicate { $0.isDefault == true })
         let defaultCategories = try context.fetch(descriptor)
         
-        guard let category = defaultCategories.first(where: { $0.transactionType == transactionType }) else { throw AppError.noDefaultCategory }
+        guard let category = defaultCategories.first(where: { $0.transactionType == type }) else { throw AppError.noDefaultCategory }
         
         return category
     }
